1443028b4f819217356b8e575fc5c4d4a852d540
Forrest Reiland <you@example.com>
2020-08-19 16:30:02 -0700

    remove excess code, try to get for loop working

--- js/scripts.js
+++ js/scripts.js
@@ -17,19 +17,48 @@ function pigVowel(word) {
     
     
     
+ 
+
+    function testFunc(word) {
+      let wordArr = word.split();
+      for (let i=0; i < wordArr.length; i++) {
+        
+          console.log(wordArr[i]);
+
+        
+      }
+    }
+
+
     function pigConsonants(word) {
       let consArray = word.split("");
       for (let i=0; i < consArray.length; i++) {
-        if (consArray[i] != (/[aeiouy]/gi)) {
+        if (consArray[i] != (/[aeiouy]*/i)) {
           consArray.push(consArray[i]);
           let consGone = [consArray.shift()];
           consFinal = consArray.join("");
+        } else {
+          break;
         }
       }
       return consFinal;
     }
+
+
+
+
+    function pigConsonants(word) {
+      let consArray = word.split("");
+      let newArr = [];
+      for (let i=0; i < consArray.length; i++) {
+        if (consArray[i] === (/[el]]/gi)) {
+         console.log("snead")
+        };
+      }
+      return newArr;
+    }
       
-    
+
 
       
     
