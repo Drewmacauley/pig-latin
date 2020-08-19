d2937beb2dfcad962cfcf95ddd367212d7b0c2d3
Forrest Reiland <you@example.com>
2020-08-19 14:37:05 -0700

    add y test function to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,29 @@ function pigVowel(word) {
   if (word.charAt(0) === ('a' || 'e' || "i" || "o" || "u"));
   return word.concat("way");
 }
+  
+         
+  function yWord(word) {
+    if (word.charAt(0) === ("y"));
+    let yArr = word.split("");
+    let yGone = [yArr.shift()];
+     yArr.push("y", "a", "y");
+     yFinal = yArr.join("");
+    return yFinal;}
+    
 
+    function pigConsonants(word) {
+      let consArr = [];
+      if (word.charAt(0) != ('a' || 'e' || "i" || "o" || "u" || "y"));
+      for (let 1=0; i < consArr.length; i++) {
+        
+      }
+      
+      let consArr = word.split("");
+      let yGone = [yArr.shift()];
+       yArr.push("y", "a", "y");
+       yFinal = yArr.join("");
+      return yFinal;}
+
+    }
+}
